cba6c7d02a3f2c8439f1354bb1c720695123321e
Shubham <38177776+shubham-11700069@users.noreply.github.com>
2021-06-13 17:13:09 +0530

    removed sublime text

--- js/main.js
+++ js/main.js
@@ -13,11 +13,6 @@
 
     function tagsGenerator() {
 
-Application SW –  
-Programming Skills – 
-Other Skills – 
-
-
         var skillsApplication = ['Power BI', 'Tableau', 'Jira', 'Salesforce', 'DBeaver', 'Postman', 'Putty', 'IntelliJ', 'Azure Storage Explorer', 'AWS s3', 'Azure Portal', 'Azure Databricks', 'Azure Data Factory', 'GitHub', 'Grafana', 'Apache Zeppelin', 'Superset', 'Teams', 'Outlook', 'Excel', 'Word', 'PowerPoint'],
             skillsProgramming = ['Python', 'pandas', 'numpy', 'matplotlib', 'SQL', 'PySpark', 'Spark', 'Hive', 'Scala', 'Shell', 'R', 'C', 'C++', 'Java', 'PIG'],
             skills3 = ['Data Visualization', 'Hadoop', 'Presto', 'Data and Business Analytics', 'Advanced Statistics and Predictive Modelling', 'Stakeholder Management', 'Project Management', 'Machine Learning', 'avsc', 'parquet', 'snappy file formats'],
