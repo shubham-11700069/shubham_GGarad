d272638321586723ddfcff5791b213c04b059c48
Shubham <38177776+shubham-11700069@users.noreply.github.com>
2021-06-14 00:47:44 +0530

    adding semicolons to end of skills line

--- js/main.js
+++ js/main.js
@@ -13,10 +13,10 @@
 
     function tagsGenerator() {
 
-        var skillsApp = ['Power BI', 'Tableau', 'Jira', 'Salesforce', 'DBeaver', 'Postman', 'Putty', 'IntelliJ', 'Azure Storage Explorer', 'AWS s3', 'Azure Portal', 'Azure Databricks', 'Azure Data Factory', 'GitHub', 'Grafana', 'Apache Zeppelin', 'Superset', 'Teams', 'Outlook', 'Excel', 'Word', 'PowerPoint']
-        var skillsPro = ['Python', 'pandas', 'numpy', 'matplotlib', 'SQL', 'PySpark', 'Spark', 'Hive', 'Scala', 'Shell', 'R', 'C', 'C++', 'Java', 'PIG']
-        var skillsOther = ['Data Visualization', 'Hadoop', 'Presto', 'Data and Business Analytics', 'Advanced Statistics and Predictive Modelling', 'Stakeholder Management', 'Project Management', 'Machine Learning', 'avsc', 'parquet', 'snappy file formats']
-        var colors = ['btn-info', 'btn-success', 'btn-danger', 'btn-primary', 'btn-warning'],
+        var skillsApp = ['Power BI', 'Tableau', 'Jira', 'Salesforce', 'DBeaver', 'Postman', 'Putty', 'IntelliJ', 'Azure Storage Explorer', 'AWS s3', 'Azure Portal', 'Azure Databricks', 'Azure Data Factory', 'GitHub', 'Grafana', 'Apache Zeppelin', 'Superset', 'Teams', 'Outlook', 'Excel', 'Word', 'PowerPoint'];
+        var skillsPro = ['Python', 'pandas', 'numpy', 'matplotlib', 'SQL', 'PySpark', 'Spark', 'Hive', 'Scala', 'Shell', 'R', 'C', 'C++', 'Java', 'PIG'];
+        var skillsOther = ['Data Visualization', 'Hadoop', 'Presto', 'Data and Business Analytics', 'Advanced Statistics and Predictive Modelling', 'Stakeholder Management', 'Project Management', 'Machine Learning', 'avsc', 'parquet', 'snappy file formats'];
+        var colors = ['btn-info', 'btn-success', 'btn-danger', 'btn-primary', 'btn-warning'];
         var i = 0;
 
         $.each(skillsApp, function(key, value) {
